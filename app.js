05fa481f28b3bb1a99e5557e457d4324ec73977d
adriennn <adrien.vetterli@gmail.com>
2017-09-14 11:16:59 +0300

    moved app.js code to ES6

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-var compression = require('compression'),
-    express = require('express'),
-    path = require('path'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
-    jayson = require('jayson'),
-    csp = require('helmet-csp'),
-    bodylogger = require('morgan-body');
+const compression = require('compression')
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const jayson = require('jayson')
+const csp = require('helmet-csp')
+const bodylogger = require('morgan-body')
 
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var app = express()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
 
-bodylogger(app);
+bodylogger(app)
 
-var mainroute = require('./routes/index'),
-    formroute = require('./routes/form'),
-    rpcroute = require('./routes/rpc');
+const mainroute = require('./routes/index')
+const formroute = require('./routes/form')
+const rpcroute = require('./routes/rpc')
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-app.set('trust proxy', '127.0.0.1');
-app.set('subdomain offset', 2);
-app.set('json replacer', ' ');
-app.set('json space', 4);
-app.set('socketio', io);
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+// Trust this proxy for sockets
+app.set('trust proxy', '127.0.0.1')
+app.set('subdomain offset', 2)
+app.set('json replacer', ' ')
+app.set('json space', 4)
+app.set('socketio', io)
 
 // attach third-party middleware to app root
 app.use(csp({
@@ -34,52 +35,50 @@ app.use(csp({
     styleSrc: ["'self'", "'unsafe-inline'", 'https://garbagepla.net', 'https://cdnjs.cloudflare.com', 'https://rawgit.com'],
     scriptSrc: ["'self'", 'cdnjs.cloudflare.com', "'unsafe-inline'", 'https://garbagepla.net', 'https://cdnjs.cloudflare.com', 'https://rawgit.com']
   }
-}));
-app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+}))
+
+app.use(compression())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // attach routes
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/lora/', mainroute);
-app.use('/lora/form', formroute);
-app.use('/lora/rpc', rpcroute);
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/lora/', mainroute)
+app.use('/lora/form', formroute)
+app.use('/lora/rpc', rpcroute)
 
-// setup sockets
-io.sockets.on('connection', function (socket) {
+// setup socketio
+io.sockets.on('connection', (socket) => {
+  console.log('client connect')
+})
 
-    console.log('client connect');
-
-});
-
-app.use(function (req, res, next) {
-  // set the sock in the req so we can access it anywhere
-  req.io = io;
-  next();
-});
+app.use((req, res, next) => {
+  req.io = io
+  next()
+})
 
 // error handler
-// app.use(function (err, req, res, next) {
+// app.use((err, req, res, next) => {
 //   // set locals, only providing error in development
-//   res.locals.path = req.path;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   res.locals.path = req.path
+//   res.locals.error = req.app.get('env') === 'development' ? err : {}
 //   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+//   res.status(err.status || 500)
+//   res.render('error')
+// })
 //
 // // catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   var err = new Error('Express: Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+// app.use((req, res, next) => {
+//   var err = new Error('Express: Not Found')
+//   err.status = 404
+//   next(err)
+// })
 
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   console.log('error: ', err)
   res.status(500).send('Something broke!')
 })
 
-// export both app and server to be able to use socketio in req and res everywhere
-module.exports = {app: app, server: server};
-console.log('app started at http://localhost:5000/lora');
+// export both app and server to be enable the use of socketio in req and res everywhere
+module.exports = {app: app, server: server}
+console.log('app started at http://localhost:5000/lora')
