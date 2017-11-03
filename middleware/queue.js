e6a84469cd0c67c64460cc9c4af35e46f805605b
adriennn <adrien.vetterli@gmail.com>
2017-11-03 13:25:41 +0100

    disable auth for queues

--- middleware/queue.js
+++ middleware/queue.js
@@ -6,6 +6,6 @@ const redis_password = process.env.REDIS_PWD
 const RSMQPromise    = require('rsmq-promise');
 const rsmq           = new RSMQPromise( {host: redis_url, port: redis_port, ns: "rsmq"} )
 
-if (process.env.NODE_ENV === 'production') rsmq.auth(redis_password)
+// if (process.env.NODE_ENV === 'production') rsmq.auth(redis_password)
 
 module.exports = rsmq
