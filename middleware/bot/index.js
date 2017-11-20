edc96f2f702ffb46e04cb8d1a571a72cdf97244b
adriennn <adrien.vetterli@gmail.com>
2017-11-20 23:42:28 +0200

    removed botmaster

--- middleware/bot/index.js
+++ middleware/bot/index.js
@@ -1,22 +1,5 @@
-// const TelegramBotClient = require('telegram-bot-client')
-
-// const client = new TelegramBotClient(process.env.TELEGRAM_TOKEN)
-const path   = require('path')
-
-const {server} = require(path.join(__dirname,'../../app'))
-
-const Botmaster   = require('botmaster')
-const TelegramBot = require('botmaster-telegram')
-const botmaster   = new Botmaster({server: server})
-
-// Setup the telegram bot
-const telegramSettings = {}
-      telegramSettings.credentials = {}
-      telegramSettings.credentials.authToken = process.env.TELEGRAM_TOKEN
-      telegramSettings.webhookEndpoint = '/webhook' + process.env.TELEGRAM_WEBHOOK_ENDPOINT_HASH
-
-const telegrambot = new TelegramBot(telegramSettings)
-botmaster.addBot(telegrambot)
+const TelegramBotClient = require('telegram-bot-client')
+const client = new TelegramBotClient(process.env.TELEGRAM_TOKEN)
 
 /*
  * This middleware allows the Telegram bot to interact with the app
@@ -26,26 +9,14 @@ module.exports = (req, res, next) => {
 
     console.log('hit bot controller: ', req.body)
 
-    try {
+    let chatid= req.body.message.chat.id
 
-      botmaster.use({
-        type: 'incoming',
-        name: 'my-middleware',
-        controller: (bot, update) => {
-          return bot.reply(update, 'Hello world!');
-        }
-      })
+    try  {
 
-  }
-
-    // let chatid= req.body.message.chat.id
-    //
-    // try  {
-    //
-    //     client.sendMessage(chatid, 'Oh hi Mark');
-    //     // return res.status(200).send('OK')
-    //     res.end()
-    // }
+        client.sendMessage(chatid, 'Oh hi Mark');
+        // return res.status(200).send('OK')
+        res.end()
+    }
 
     catch (err) {
 
